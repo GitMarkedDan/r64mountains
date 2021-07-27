40914c8bd2c1db7f3cd41da8d9c43c8217a181e7
Justtryingtmakecode <77183602+Justtryingtmakecode@users.noreply.github.com>
2021-07-27 21:35:30 +0800

    Update code.lua
    c

--- code.lua
+++ code.lua
@@ -884,8 +884,9 @@ function clear()
     stuff.gd[5] = string.sub(stuff.gd[5],0,64)
     game:GetService("Workspace").share.save:FireServer(stuff.gd)
 end
-createButton("Clear", UDim2.new(0.75,0,0,0),"Clear Extra ICs", clear, 0.75)
-
+if not game.Players.LocalPlayer.PlayerGui.UI.pause.bg.pause:FindFirstChild("Clear") then
+    createButton("Clear", UDim2.new(0.75,0,0,0),"Clear Extra ICs", clear, 0.75) 
+end
 sign = game:GetService("ReplicatedFirst").maps.hub["Sheldon Sign"]
 sign.tx.Value = "Hey, I added a thing in your pause menu."
 sign.tx.tx.Value = "You gotta press it and then quick to menu."
